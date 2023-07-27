9c73c14e9c5a1504b9db3624c6d94b2e5ea3c99e
ibuprofen99 <kouis20@yahoo.com>
2023-07-26 17:44:39 -0700

    added basic js

---
+++ app.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    const roomsTab = document.querySelector('a[href="rooms.html"]');
+    const AmenitiesTab = document.querySelector('a[href="amenities.html"]');
+    const IndexTab = document.querySelector('a[href="index.html"]');
+  
+    roomsTab.addEventListener("click", function(event) {
+
+      });
+    AmenitiesTab.addEventListener("click", function(event) {
+
+      });
+
+    IndexTab.addEventListener("click", function(event) {
+       
+      });
+
+  });
+  
