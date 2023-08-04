6df9ecee48d1d929ff6d605d83cf146ca1ddd4bf
NoShotFlip <139315237+NoShotFlip@users.noreply.github.com>
2023-08-03 18:51:19 -0700

    Added to DataBase

--- javaFiles/Database.java
+++ javaFiles/Database.java
@@ -37,17 +37,14 @@ public class Database {
         con.close(); // close connection
        // System.out.println("Connection Closed....");
     }
-    static void addtoCustomerDetails() throws ClassNotFoundException, SQLException{
-        String ftest =  "john";
-        String ltest= "smith";
-        String etest= "example@yahoo.com";
-        int itest = 2;
+    static void addtoCustomerDetails(String firstName, String lastName ,String email, int roomNum ) throws ClassNotFoundException, SQLException{
+       
         String url
             = "jdbc:mysql://hotel-reservation.cxhswfveunmf.us-west-1.rds.amazonaws.com:3306/Hotel_Reservation"; // table details
         String username = "admin"; // MySQL credentials
         String password = "01NoShotFlip$";
          String query
-            = "insert into Customer_Details(ID,First_Name,Last_Name,Email) "+"values (?, ?, ?, ?)"; // query to be run
+            = "insert into Customer_Details(RoomNum,First_Name,Last_Name,Email) "+"values (?, ?, ?, ?)"; // query to be run
         Class.forName(
             "com.mysql.cj.jdbc.Driver"); // Driver name
         Connection con = DriverManager.getConnection(
@@ -55,18 +52,161 @@ public class Database {
         
         //Statement st = con.createStatement();
         PreparedStatement preparedStmt = con.prepareStatement(query);
-        preparedStmt.setInt (1, itest);
-        preparedStmt.setString (2, ftest);
-        preparedStmt.setString (3, ltest);
-        preparedStmt.setString (4, etest);
+        preparedStmt.setInt (1, roomNum);
+        preparedStmt.setString (2, firstName);
+        preparedStmt.setString (3, lastName);
+        preparedStmt.setString (4, email);
         preparedStmt.execute();
         
         //ResultSet rs
           //  = st.executeQuery(query);
-        System.out.println("ADDED");
+        //System.out.println("ADDED");
         //rs.next(); 
         //st.close(); // close statement
         con.close(); // close connection   
+    }
+    void deleteCustomerDetails(int roomNum) throws ClassNotFoundException, SQLException{
+         String url
+            = "jdbc:mysql://hotel-reservation.cxhswfveunmf.us-west-1.rds.amazonaws.com:3306/Hotel_Reservation"; // table details
+        String username = "admin"; // MySQL credentials
+        String password = "01NoShotFlip$";
+        String query
+            = "DELETE FROM Customer_Details WHERE RoomNum = ?"; // query to be run
+        Class.forName(
+            "com.mysql.cj.jdbc.Driver"); // Driver name
+        Connection con = DriverManager.getConnection(
+            url, username, password);
+        PreparedStatement preparedStmt = con.prepareStatement(query);
+        preparedStmt.setInt (1, roomNum);
+        preparedStmt.executeUpdate();
+        con.close(); // close connection  
+   }
+   
+    void roomAvailability() throws ClassNotFoundException, SQLException{
+        String url
+            = "jdbc:mysql://hotel-reservation.cxhswfveunmf.us-west-1.rds.amazonaws.com:3306/Hotel_Reservation"; // table details
+        String username = "admin"; // MySQL credentials
+        String password = "01NoShotFlip$";
+        String query
+            = "select *from Room_Inventory"; // query to be run
+         Class.forName(
+            "com.mysql.cj.jdbc.Driver"); // Driver name
+        Connection con = DriverManager.getConnection(
+            url, username, password); 
+        Statement st = con.createStatement();
+        ResultSet rs
+            = st.executeQuery(query); // Execute query
+        while(rs.next()){
+             int roomNum = rs.getInt("RoomNum");
+            String availability
+             = rs.getString("Availability"); // Retrieve name from db
+            String roomType = rs.getString("Room_Type");
+            
+            System.out.println(roomNum +" "+availability +" "+roomType);
+
+        }
+        st.close(); // close statement
+        con.close(); // close connection
+    }
+    void updateRoomAvailability(int roomNum) throws SQLException, ClassNotFoundException{
+
+         String url
+            = "jdbc:mysql://hotel-reservation.cxhswfveunmf.us-west-1.rds.amazonaws.com:3306/Hotel_Reservation"; // table details
+        String username = "admin"; // MySQL credentials
+        String password = "01NoShotFlip$";
+        String query
+            = "update Room_Inventory SET Availability = Not Availability where RoomNum? ";
+         Class.forName(
+            "com.mysql.cj.jdbc.Driver"); // Driver name
+        Connection con = DriverManager.getConnection(
+            url, username, password);
+        
+        //Statement st = con.createStatement();
+        PreparedStatement preparedStmt = con.prepareStatement(query);
+        preparedStmt.setInt (1, roomNum);
+        preparedStmt.executeUpdate();
+        con.close(); // close connection  
+    }
+    void nowAvailabe(int roomNum) throws ClassNotFoundException, SQLException{
+          String url
+            = "jdbc:mysql://hotel-reservation.cxhswfveunmf.us-west-1.rds.amazonaws.com:3306/Hotel_Reservation"; // table details
+        String username = "admin"; // MySQL credentials
+        String password = "01NoShotFlip$";
+        String query
+            = "update Room_Inventory SET Availability =  Availability where RoomNum? ";
+         Class.forName(
+            "com.mysql.cj.jdbc.Driver"); // Driver name
+        Connection con = DriverManager.getConnection(
+            url, username, password);
+        
+        //Statement st = con.createStatement();
+        PreparedStatement preparedStmt = con.prepareStatement(query);
+        preparedStmt.setInt (1, roomNum);
+        preparedStmt.executeUpdate();
+        con.close(); // close connection  
+    }
+    
+    
+    void bookingDetails() throws ClassNotFoundException, SQLException{
+        String url
+            = "jdbc:mysql://hotel-reservation.cxhswfveunmf.us-west-1.rds.amazonaws.com:3306/Hotel_Reservation"; // table details
+        String username = "admin"; // MySQL credentials
+        String password = "01NoShotFlip$";
+        String query
+            = "select *from Booking"; // query to be run
+         Class.forName(
+            "com.mysql.cj.jdbc.Driver"); // Driver name
+        Connection con = DriverManager.getConnection(
+            url, username, password); 
+        Statement st = con.createStatement();
+        ResultSet rs
+            = st.executeQuery(query); // Execute query
+        while (rs.next()){
+             int roomNum = rs.getInt("RoomNum");
+            String startDate
+             = rs.getString("Start_Date"); // Retrieve name from db
+            String endDate = rs.getString("End_Date");
+            
+            System.out.println(roomNum +" "+startDate +" "+endDate);
+        }
+        st.close(); // close statement
+        con.close(); // close connection
+    }
+    void addBooking (int roomNum , String startDate , String endDate) throws ClassNotFoundException, SQLException{
+        String url
+            = "jdbc:mysql://hotel-reservation.cxhswfveunmf.us-west-1.rds.amazonaws.com:3306/Hotel_Reservation"; // table details
+        String username = "admin"; // MySQL credentials
+        String password = "01NoShotFlip$";
+         String query
+            = "insert into Booking(RoomNum,Start_Date,End_Date) "+"values (?, ?, ?, )"; // query to be run
+
+         Class.forName(
+            "com.mysql.cj.jdbc.Driver"); // Driver name
+        Connection con = DriverManager.getConnection(
+            url, username, password);
+        PreparedStatement preparedStmt = con.prepareStatement(query);
+        preparedStmt.setInt (1, roomNum);
+        preparedStmt.setString (2, startDate);
+        preparedStmt.setString (3, endDate);
+        preparedStmt.execute();
+        con.close(); // close connection 
+
+    }
+    void deleteBooking(int roomNum) throws SQLException, ClassNotFoundException{
+         String url
+            = "jdbc:mysql://hotel-reservation.cxhswfveunmf.us-west-1.rds.amazonaws.com:3306/Hotel_Reservation"; // table details
+        String username = "admin"; // MySQL credentials
+        String password = "01NoShotFlip$";
+        String query
+            = "DELETE FROM Booking WHERE RoomNum = ?"; // query to be run
+        Class.forName(
+            "com.mysql.cj.jdbc.Driver"); // Driver name
+        Connection con = DriverManager.getConnection(
+            url, username, password);
+        PreparedStatement preparedStmt = con.prepareStatement(query);
+        preparedStmt.setInt (1, roomNum);
+        preparedStmt.executeUpdate();
+        con.close(); // close connection 
     }
 
 }
