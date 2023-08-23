d477a3652e96cfcf00c2fa90991fb084f83c86ba
ibuprofen99 <kouis20@yahoo.com>
2023-08-23 14:12:00 -0700

    update

--- javaFiles/Database.java
+++ javaFiles/Database.java
@@ -1,14 +1,27 @@
-package Hotel_Reservation.src;
+package Hotel_Toad;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.sql.*;
 
+/**
+ * Provides utilities for interacting with the hotel reservation database.
+ * @author Toad's Taskforce
+ * @version 1.0
+ * <p>
+ * Date of Creation: 28 July 2023
+ * </p>
+ */
 public class Database {
-     public static DefaultTableModel getCustomerDetailsModel() throws ClassNotFoundException, SQLException {
+
+    /**
+     * Retrieves customer details from the database and constructs a table model with this data.
+     *
+     * @return the constructed table model with customer details.
+     * @throws ClassNotFoundException if the database driver is not found.
+     * @throws SQLException if there's any database access error.
+     */
+    public static DefaultTableModel getCustomerDetailsModel() throws ClassNotFoundException, SQLException {
             String url = "jdbc:mysql://hotel-reservation.cxhswfveunmf.us-west-1.rds.amazonaws.com:3306/Hotel_Reservation";
             String username = "admin";
             String password = "01NoShotFlip$";
@@ -43,8 +56,19 @@ public class Database {
 
             return model;
         }
-    static void addtoCustomerDetails(String firstName, String lastName ,String email, int roomNum ) throws ClassNotFoundException, SQLException{
-       
+
+    /**
+     * Adds a customer to the database with the provided details.
+     *
+     * @param firstName The first name of the customer.
+     * @param lastName The last name of the customer.
+     * @param email The email of the customer.
+     * @param number The room number of the customer.
+     * @throws ClassNotFoundException if the database driver is not found.
+     * @throws SQLException if there's any database access error.
+     */
+    static void addtoCustomerDetails(String firstName, String lastName ,String email, int number ) throws ClassNotFoundException, SQLException{
+        
         String url
             = "jdbc:mysql://hotel-reservation.cxhswfveunmf.us-west-1.rds.amazonaws.com:3306/Hotel_Reservation"; // table details
         String username = "admin"; // MySQL credentials
@@ -58,7 +82,7 @@ public class Database {
         
         //Statement st = con.createStatement();
         PreparedStatement preparedStmt = con.prepareStatement(query);
-        preparedStmt.setInt (1, roomNum);
+        preparedStmt.setInt (1, number);
         preparedStmt.setString (2, firstName);
         preparedStmt.setString (3, lastName);
         preparedStmt.setString (4, email);
@@ -71,7 +95,15 @@ public class Database {
         //st.close(); // close statement
         con.close(); // close connection   
     }
-    void deleteCustomerDetails(int roomNum) throws ClassNotFoundException, SQLException{
+
+    /**
+     * Deletes customer details associated with the provided room number.
+     *
+     * @param roomNum The room number of the customer to be deleted.
+     * @throws ClassNotFoundException if the database driver is not found.
+     * @throws SQLException if there's any database access error.
+     */
+    static void deleteCustomerDetails(int roomNum) throws ClassNotFoundException, SQLException{
          String url
             = "jdbc:mysql://hotel-reservation.cxhswfveunmf.us-west-1.rds.amazonaws.com:3306/Hotel_Reservation"; // table details
         String username = "admin"; // MySQL credentials
@@ -86,9 +118,16 @@ public class Database {
         preparedStmt.setInt (1, roomNum);
         preparedStmt.executeUpdate();
         con.close(); // close connection  
-   }
-   
-   public static DefaultTableModel getRoomAvailabilityModel() throws ClassNotFoundException, SQLException {
+    }
+
+    /**
+     * Retrieves room availability from the database and constructs a table model with this data.
+     *
+     * @return the constructed table model with room availability details.
+     * @throws ClassNotFoundException if the database driver is not found.
+     * @throws SQLException if there's any database access error.
+     */
+    public static DefaultTableModel getRoomAvailabilityModel() throws ClassNotFoundException, SQLException {
             String url = "jdbc:mysql://hotel-reservation.cxhswfveunmf.us-west-1.rds.amazonaws.com:3306/Hotel_Reservation";
             String username = "admin";
             String password = "01NoShotFlip$";
@@ -123,14 +162,22 @@ public class Database {
 
             return model;
         }
-    void updateRoomAvailability(int roomNum) throws SQLException, ClassNotFoundException{
 
+    /**
+     * Updates room availability in the database to 'Not Available' for the specified room number.
+     *
+     * @param number The room number to be updated.
+     * @throws ClassNotFoundException if the database driver is not found.
+     * @throws SQLException if there's any database access error.
+     */
+    static void updateRoomAvailability(int number) throws SQLException, ClassNotFoundException{
+        
          String url
             = "jdbc:mysql://hotel-reservation.cxhswfveunmf.us-west-1.rds.amazonaws.com:3306/Hotel_Reservation"; // table details
-        String username = "admin"; // MySQL credentials
+        String  username = "admin"; // MySQL credentials
         String password = "01NoShotFlip$";
         String query
-            = "update Room_Inventory SET Availability = 'Not Availability' where RoomNum = ? ";
+            = "update Room_Inventory SET Availability = 'Not Available' where RoomNum = ?  ";
          Class.forName(
             "com.mysql.cj.jdbc.Driver"); // Driver name
         Connection con = DriverManager.getConnection(
@@ -138,17 +185,25 @@ public class Database {
         
         //Statement st = con.createStatement();
         PreparedStatement preparedStmt = con.prepareStatement(query);
-        preparedStmt.setInt (1, roomNum);
+        preparedStmt.setInt (1, number);
         preparedStmt.executeUpdate();
         con.close(); // close connection  
     }
-    void nowAvailabe(int roomNum) throws ClassNotFoundException, SQLException{
+
+    /**
+     * Updates room availability in the database to 'Available' for the specified room number.
+     *
+     * @param roomNum The room number to be updated.
+     * @throws ClassNotFoundException if the database driver is not found.
+     * @throws SQLException if there's any database access error.
+     */
+    static void nowAvailable(int roomNum) throws ClassNotFoundException, SQLException{
           String url
             = "jdbc:mysql://hotel-reservation.cxhswfveunmf.us-west-1.rds.amazonaws.com:3306/Hotel_Reservation"; // table details
         String username = "admin"; // MySQL credentials
         String password = "01NoShotFlip$";
         String query
-            = "update Room_Inventory SET Availability =  'Availability' where RoomNum = ? ";
+            = "update Room_Inventory SET Availability =  'Available' where RoomNum =? ";
          Class.forName(
             "com.mysql.cj.jdbc.Driver"); // Driver name
         Connection con = DriverManager.getConnection(
@@ -160,8 +215,14 @@ public class Database {
         preparedStmt.executeUpdate();
         con.close(); // close connection  
     }
-    
-    
+
+    /**
+     * Retrieves booking details from the database and constructs a table model with this data.
+     *
+     * @return the constructed table model with booking details.
+     * @throws ClassNotFoundException if the database driver is not found.
+     * @throws SQLException if there's any database access error.
+     */
     public static DefaultTableModel bookingDetails() throws ClassNotFoundException, SQLException {
         String url = "jdbc:mysql://hotel-reservation.cxhswfveunmf.us-west-1.rds.amazonaws.com:3306/Hotel_Reservation";
         String username = "admin";
@@ -197,7 +258,18 @@ public class Database {
     
         return model;
     }
-    void addBooking (int roomNum , String startDate , String endDate) throws ClassNotFoundException, SQLException{
+
+    /**
+     * Adds a booking in the database with the provided details.
+     *
+     * @param number The room number associated with the booking.
+     * @param startDate The start date of the booking.
+     * @param endDate The end date of the booking.
+     * @throws ClassNotFoundException if the database driver is not found.
+     * @throws SQLException if there's any database access error.
+     */
+    static void addBooking (int number , String startDate , String endDate) throws ClassNotFoundException, SQLException{
+       
         String url
             = "jdbc:mysql://hotel-reservation.cxhswfveunmf.us-west-1.rds.amazonaws.com:3306/Hotel_Reservation"; // table details
         String username = "admin"; // MySQL credentials
@@ -210,14 +282,22 @@ public class Database {
         Connection con = DriverManager.getConnection(
             url, username, password);
         PreparedStatement preparedStmt = con.prepareStatement(query);
-        preparedStmt.setInt (1, roomNum);
+        preparedStmt.setInt (1, number);
         preparedStmt.setString (2, startDate);
         preparedStmt.setString (3, endDate);
         preparedStmt.execute();
         con.close(); // close connection 
 
     }
-    void deleteBooking(int roomNum) throws SQLException, ClassNotFoundException{
+
+    /**
+     * Deletes booking details associated with the provided room number.
+     *
+     * @param roomNum The room number of the booking to be deleted.
+     * @throws ClassNotFoundException if the database driver is not found.
+     * @throws SQLException if there's any database access error.
+     */
+    static void deleteBooking(int roomNum) throws SQLException, ClassNotFoundException{
          String url
             = "jdbc:mysql://hotel-reservation.cxhswfveunmf.us-west-1.rds.amazonaws.com:3306/Hotel_Reservation"; // table details
         String username = "admin"; // MySQL credentials
@@ -233,5 +313,4 @@ public class Database {
         preparedStmt.executeUpdate();
         con.close(); // close connection 
     }
-
 }
--- javaFiles/Guest.java
+++ javaFiles/Guest.java
@@ -1,38 +1,64 @@
-package Hotel_Reservation.src;
+package Hotel_Toad;
 
-// Java Guest class
+/**
+ * Represents a guest with associated personal details.
+ * @author Toad's Taskforce
+ * @version 1.0
+ * <p>
+ * Date of Creation: 28 July 2023
+ * </p>
+ *
+ * This class provides functionalities to manage and retrieve guest details.
+ */
 public class Guest {
 
-    // Guest class attributes
+    /** The first name of the guest. */
     private String firstName;
+    /** The last name of the guest. */
     private String lastName;
+    /** The email address of the guest. */
     private String email;
 
 
 
-    // Guest class constructor
+    /**
+     * Constructs a new Guest with the given personal details.
+     *
+     * @param firstName the first name of the guest.
+     * @param lastName  the last name of the guest.
+     * @param email     the email address of the guest.
+     */
     public Guest(String firstName, String lastName, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
     }
 
-    // Getters for Guest class
+    /**
+     * Retrieves the first name of the guest.
+     *
+     * @return the first name of the guest.
+     */
     public String getFirstName(){
         return firstName;
     }
 
+    /**
+     * Retrieves the last name of the guest.
+     *
+     * @return the last name of the guest.
+     */
     public String getLastName(){
         return lastName;
     }
 
+    /**
+     * Retrieves the email address of the guest.
+     *
+     * @return the email address of the guest.
+     */
     public String getEmail(){
         return email;
     }
 
-
-    // Guest method for printing out Guest data
-    public String printGuestInfo(){
-        return "\nFirst Name: "+firstName+"\nLast Name: "+lastName+"\nEmail: "+email+"\n";
-    }
 }
